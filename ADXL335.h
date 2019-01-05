d6477d63e96b4982b3a9ec7fd831493ff5f750df
PrafulSaravanan <prafulsaravanan@gmail.com>
2019-01-06 00:33:58 +0530

    Update ADXL335.h

--- ADXL335.h
+++ ADXL335.h
@@ -39,10 +39,10 @@
 
 #define ADC_AMPLITUDE 1024//amplitude of the 10bit-ADC of Arduino is 1024LSB
 #define ADC_REF 5   //ADC reference is 5v
-#define ZERO_X  1.57 //accleration of X-AXIS is 0g, the voltage of X-AXIS is 1.22v
-#define ZERO_Y  1.26 //
-#define ZERO_Z  1.34 //
-#define SENSITIVITY 0.3//sensitivity of X/Y/Z axis is 0.25v/g
+#define ZERO_X  1.35 //accleration of X-AXIS is 0g, the voltage of X-AXIS is 1.22v
+#define ZERO_Y  1.35 //
+#define ZERO_Z  1.2 //
+#define SENSITIVITY 0.27//sensitivity of X/Y/Z axis is 0.25v/g
 
 class ADXL335{
 private:
